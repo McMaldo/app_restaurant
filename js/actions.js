647916c696496c3a8f1d158255109017ac515d95
Maldonado Pablo Ignacio <164094204+McMaldo@users.noreply.github.com>
2024-05-14 19:57:55 -0300

    aplicación de DOM a app resto

---
+++ js/actions.js
@@ -0,0 +1,13 @@
+console.log("prueba");
+
+let comida = document.getElementById("comida");
+console.log(comida);
+
+let titulo = document.querySelector(".icons")
+console.log(titulo);
+
+let lista = document.querySelector("li")
+console.log(lista);
+
+let listas = document.querySelectorAll("li")
+console.log(listas);
